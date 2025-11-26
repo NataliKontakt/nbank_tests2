c3780bafab5d73a64729c81acf6edb2b8e1077d8
natali-kontakt <natalikontaktgood@gmail.com>
2025-11-26 21:12:04 +0500

    Temp commit with all files

---
+++ src/test/java/iteration2/DepositTest.java
@@ -0,0 +1,99 @@
+package iteration2;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+public class DepositTest {
+    @BeforeAll
+    public static void setupRestAssured() {
+        RestAssured.filters(
+                List.of(new RequestLoggingFilter(),
+                        new ResponseLoggingFilter()));
+    }
+
+    @Test
+    public void userCanMakeDepositTest() {
+//        Создать пользователя
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", "Basic YWRtaW46YWRtaW4=")
+                .body("""
+                        {
+                          "username": "kate2005",
+                          "password": "Kate2000#",
+                          "role": "USER"
+                        }
+                        """)
+                .post("http://localhost:4111/api/v1/admin/users")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED);
+        // получаем токен юзера
+        String userAuthHeader = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("""
+                        {
+                          "username": "kate2005",
+                          "password": "Kate2000#"
+                        }
+                        """)
+                .post("http://localhost:4111/api/v1/auth/login")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .header("Authorization");
+
+        // создаем аккаунт(счет)
+        Response response = given()
+                .header("Authorization", userAuthHeader)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .post("http://localhost:4111/api/v1/accounts")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED)
+                .extract()
+                .response();
+
+        int id = response.path("id");
+        float balance = response.path("balance");
+
+        System.out.println("ID = " + id);
+        System.out.println("Balance = " + balance);
+
+        // вносим депозит
+        float deposit = 50;
+        String body = String.format(Locale.US, """
+        {
+          "id": %d,
+          "balance": %f
+        }
+        """, id, deposit);
+        given()
+                .header("Authorization", userAuthHeader)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(body)
+                .post("http://localhost:4111/api/v1/accounts/deposit")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+
+}
