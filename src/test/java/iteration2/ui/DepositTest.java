c36648950e0a28ea97a91745f5908018f830991e
natali-kontakt <natalikontaktgood@gmail.com>
2026-01-03 09:12:28 +0500

    add DepositTest

--- src/test/java/iteration2/ui/DepositTest.java
+++ src/test/java/iteration2/ui/DepositTest.java
@@ -180,4 +180,138 @@ public class DepositTest {
 
         assertThat(accountResponse.getBalance()).isZero();
     }
+
+    @Test
+    public void userCanNotDepositAccountTestMore5000() throws AccountNotFoundException {
+        // ШАГИ ПО НАСТРОЙКЕ ОКРУЖЕНИЯ
+        // ШАГ 1: админ логинится в банке
+        // ШАГ 2: админ создает юзера
+        // ШАГ 3: юзер логинится в банке
+        // ШАГ 4: юзер создает аккаунт
+
+        CreateUserRequest user = AdminSteps.createUser();
+        CreateAccountResponse account = UserSteps.createAccount(user.getUsername(), user.getPassword());
+        String accountNumber = account.getAccountNumber();
+
+        String userAuthHeader = new CrudRequester(
+                RequestSpec.unauthSpec(),
+                Endpoint.LOGIN,
+                ResponseSpec.requestReturnsOk())
+                .post(LoginRequest.builder().username(user.getUsername()).password(user.getPassword()).build())
+                .extract()
+                .header("Authorization");
+
+        Selenide.open("/");
+
+        executeJavaScript("localStorage.setItem('authToken', arguments[0]);", userAuthHeader);
+
+        Selenide.open("/dashboard");
+
+        // ШАГИ ТЕСТА
+        // ШАГ 5: юзер нажимает 💰 Deposit Money
+        float deposit = RandomData.getDeposit() + 5000;
+        $(Selectors.byText("💰 Deposit Money")).click();
+        $((".account-selector")).click();
+        $(Selectors.byText(accountNumber)).click();
+        $(Selectors.byAttribute("placeholder", "Enter amount")).sendKeys(String.valueOf(deposit));
+        $(Selectors.byText("💵 Deposit")).click();
+
+        // ШАГ 6: проверка, что ошибка ❌ Please deposit less or equal to 5000$.
+
+        Alert alert = switchTo().alert();
+        String alertText = alert.getText();
+
+        String expectedMessage = "❌ Please deposit less or equal to 5000$.";
+        assertThat(alertText).contains(expectedMessage);
+
+        alert.accept();
+
+        // ШАГ 7: проверка, что аккаунт не был пополнен на UI
+        $(Selectors.byText("🏠 Home")).click();
+        $(Selectors.byText("🔄 Make a Transfer")).click();
+        $(Selectors.byText("🔁 Transfer Again")).click();
+        $$("li.list-group-item.d-flex.justify-content-between")
+                .shouldHave(size(0));
+
+        // ШАГ 7: проверка, что баланс аккаунта равен нулю на API
+        CreateAccountResponse[] existingUserAccounts = given()
+                .spec(RequestSpec.authSpec(user.getUsername(), user.getPassword()))
+                .get("http://localhost:4111/api/v1/customer/accounts")
+                .then().assertThat()
+                .extract().as(CreateAccountResponse[].class);
+
+        CreateAccountResponse accountResponse = Arrays.stream(existingUserAccounts).filter(
+                        accounts -> accounts.getAccountNumber().equals(accountNumber))
+                .findFirst()
+                .orElseThrow(() -> new AccountNotFoundException("Счет не найден: " + accountNumber));
+
+        assertThat(accountResponse.getBalance()).isZero();
+    }
+
+    @Test
+    public void userCanNotDepositAccountTestLessOneCent() throws AccountNotFoundException {
+        // ШАГИ ПО НАСТРОЙКЕ ОКРУЖЕНИЯ
+        // ШАГ 1: админ логинится в банке
+        // ШАГ 2: админ создает юзера
+        // ШАГ 3: юзер логинится в банке
+        // ШАГ 4: юзер создает аккаунт
+
+        CreateUserRequest user = AdminSteps.createUser();
+        CreateAccountResponse account = UserSteps.createAccount(user.getUsername(), user.getPassword());
+        String accountNumber = account.getAccountNumber();
+
+        String userAuthHeader = new CrudRequester(
+                RequestSpec.unauthSpec(),
+                Endpoint.LOGIN,
+                ResponseSpec.requestReturnsOk())
+                .post(LoginRequest.builder().username(user.getUsername()).password(user.getPassword()).build())
+                .extract()
+                .header("Authorization");
+
+        Selenide.open("/");
+
+        executeJavaScript("localStorage.setItem('authToken', arguments[0]);", userAuthHeader);
+
+        Selenide.open("/dashboard");
+
+        // ШАГИ ТЕСТА
+        // ШАГ 5: юзер нажимает 💰 Deposit Money
+        float deposit = RandomData.getDeposit() - 5000;
+        $(Selectors.byText("💰 Deposit Money")).click();
+        $((".account-selector")).click();
+        $(Selectors.byText(accountNumber)).click();
+        $(Selectors.byAttribute("placeholder", "Enter amount")).sendKeys(String.valueOf(deposit));
+        $(Selectors.byText("💵 Deposit")).click();
+
+        // ШАГ 6: проверка, что ошибка ❌ Please enter a valid amount.
+
+        Alert alert = switchTo().alert();
+        String alertText = alert.getText();
+
+        String expectedMessage = "❌ Please enter a valid amount.";
+        assertThat(alertText).contains(expectedMessage);
+
+        alert.accept();
+
+        // ШАГ 7: проверка, что аккаунт не был пополнен на UI
+        $(Selectors.byText("🏠 Home")).click();
+        $(Selectors.byText("🔄 Make a Transfer")).click();
+        $(Selectors.byText("🔁 Transfer Again")).click();
+        $$("li.list-group-item.d-flex.justify-content-between")
+                .shouldHave(size(0));
+
+        // ШАГ 7: проверка, что баланс аккаунта равен нулю на API
+        CreateAccountResponse[] existingUserAccounts = given()
+                .spec(RequestSpec.authSpec(user.getUsername(), user.getPassword()))
+                .get("http://localhost:4111/api/v1/customer/accounts")
+                .then().assertThat()
+                .extract().as(CreateAccountResponse[].class);
+
+        CreateAccountResponse accountResponse = Arrays.stream(existingUserAccounts).filter(
+                        accounts -> accounts.getAccountNumber().equals(accountNumber))
+                .findFirst()
+                .orElseThrow(() -> new AccountNotFoundException("Счет не найден: " + accountNumber));
+
+        assertThat(accountResponse.getBalance()).isZero();
+    }
 }
