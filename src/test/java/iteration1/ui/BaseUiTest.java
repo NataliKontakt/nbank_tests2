167b029982a4461494ce5ee820a023be4706960c
natali-kontakt <natalikontaktgood@gmail.com>
2026-01-21 21:18:49 +0500

    test_paralellization

--- src/test/java/iteration1/ui/BaseUiTest.java
+++ src/test/java/iteration1/ui/BaseUiTest.java
@@ -7,6 +7,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import common.extensions.*;
 import iteration1.api.BaseTest;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,13 +25,14 @@ public class BaseUiTest extends BaseTest {
 
     @BeforeAll
     public static void setupSelenoid() {
+
         Configuration.remote = Config.getProperty("uiRemote");
         Configuration.baseUrl = Config.getProperty("uiBaseUrl");
         Configuration.browser = Config.getProperty("browser");
         Configuration.browserSize = Config.getProperty("browserSize");
         String platform = Config.getProperty("platform");
         System.setProperty("platform", platform);
-        //Configuration.headless = true;
+        Configuration.headless = true;
         Configuration.browserCapabilities.setCapability("selenoid:options",
                 Map.of("enableVNC", true, "enableLog", true)
         );
@@ -45,5 +47,30 @@ public class BaseUiTest extends BaseTest {
     public void authAsUser(CreateUserRequest createUserRequest) {
         authAsUser(createUserRequest.getUsername(), createUserRequest.getPassword());
     }
+
+    @AfterEach
+    public void tearDown() {
+        // Принудительно закрываем браузер и очищаем
+        Selenide.closeWebDriver();
+
+        // Дополнительно: убиваем процессы Chrome
+        killChromeProcesses();
+    }
+
+    private void killChromeProcesses() {
+        try {
+            if (System.getProperty("os.name").toLowerCase().contains("win")) {
+                Runtime.getRuntime().exec("taskkill /F /IM chrome.exe");
+                Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe");
+            } else {
+                Runtime.getRuntime().exec("pkill -f chrome");
+                Runtime.getRuntime().exec("pkill -f chromedriver");
+            }
+            Thread.sleep(1000); // Даем время на завершение
+        } catch (Exception e) {
+            // Игнорируем ошибки при убийстве процессов
+        }
+        }
+
 
 }
